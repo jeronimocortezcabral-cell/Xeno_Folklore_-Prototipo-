9a922c669c5d7fda217bcde4a9ce022baef0c921
JuaniLal <jpgarsh@gmail.com>
2025-11-05 00:19:22 -0300

    Main_Menu y Pause_Menu completos funcionales
    >:)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,42 +1,58 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour
-{
+public class GameManager : MonoBehaviour {
     [SerializeField] private GameObject pauseMenu;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+    private bool isPaused = false;
+
+    void Start() {
+        if (pauseMenu != null)
+            pauseMenu.SetActive(false);
+
+        Time.timeScale = 1f;
     }
-    public void Reiniciar() 
-        {
-        Debug.Log("se iso clik");
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (isPaused)
+                Reanudar();
+            else
+                Pausar();
+        }
+    }
+
+    // --- FUNCIONES DE CONTROL DE PAUSA ---
+    public void Pausar() {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void Reanudar() {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    // --- FUNCIONES EXTRA DEL MENÚ ---
+    public void Reiniciar() {
+        Debug.Log("Reiniciando...");
+        Time.timeScale = 1f;
         SceneManager.LoadScene("MainScene");
     }
+
+    public void VolverAlMenuPrincipal() {
+        Debug.Log("Volviendo al menú principal...");
+        Time.timeScale = 1f; // Reanuda el tiempo antes de cambiar de escena
+        SceneManager.LoadScene("MainMenu"); // Cambia a la escena del menú principal
+    }
+
     public void CerrarJuego() {
         Debug.Log("Cerrando el juego...");
 #if UNITY_EDITOR
-        // Si estás en el editor, detiene el modo de juego
         UnityEditor.EditorApplication.isPlaying = false;
 #else
-    // Si estás en una build, cierra la aplicación
-    Application.Quit();
+        Application.Quit();
 #endif
-    }
-
-  
-
-    // Update is called once per frame
-    void Update()
-    {
-
-        if (Input.GetKeyDown(KeyCode.Escape))
-
-        {
-           pauseMenu.SetActive(true);
-
-        }
-        
     }
 }
--- Assets/Scripts/GameMenuManager.cs
+++ Assets/Scripts/GameMenuManager.cs
@@ -1,26 +1,30 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameMenuManager : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+public class GameMenuManager : MonoBehaviour {
+    void Start() {
+      
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
     }
 
-    public void Jugar() 
-        {
-        Debug.Log("se iso clik");
+    public void Jugar() {
+        Debug.Log("Se hizo clic en 'Jugar'");
         SceneManager.LoadScene("MainScene");
     }
-    
-    // Update is called once per frame
-    void Update()
-    {
 
+    public void SalirDelJuego() {
+        Debug.Log("Saliendo del juego...");
+#if UNITY_EDITOR
+        
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        // Si estás en una build, cierra la aplicación
+        Application.Quit();
+#endif
+    }
 
-
+    void Update() {
         
     }
 }
